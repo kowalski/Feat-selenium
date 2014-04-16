971711d9177584db6737a246dd2db8108d1c6054
Marek Kowalski <kowalski0123@gmail.com>
2014-04-16 16:29:56 +0200

    Remove obsolete code.

--- featselenium/common.py
+++ featselenium/common.py
@@ -420,15 +420,6 @@ class TestDriver(LogWrapper):
     def on_error(self, _e):
         self.do_screenshot()
 
-    def script_result(browser, script):
-        browser.execute_script(
-            "$('body').append('<div id=\"js-result\"></div>');"
-            "$('#js-result').html(%s);" % (script, ))
-        el = browser.find_element_by_xpath('//div[@id="js-result"]')
-        res = el.text
-        browser.execute_script("$('div#js-result').remove()")
-        return res
-
     def get_active_ajax(browser):
         r = browser.execute_script("return $.active")
         if isinstance(r, defer.Deferred):
